01b3e77e57401f1a38d6a82b8e9f4163276a5f37
GAS097 <work.gabriel.as@gmail.com>
2025-04-03 21:45:16 -0300

    Feito o Create, Read e o Update, agora só falta o Delete

--- projetos/todos/views.py
+++ projetos/todos/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Todo
+from django.urls import reverse_lazy
 
 # Create your views here.
-def home(request):
-    return render(request, "todos/home.html")
+class TodoListView(ListView):
+    model = Todo
+    
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy("todo_list")
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy("todo_list")
